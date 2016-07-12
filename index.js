a31366f776cf3c89066486e26309ec6d3cba5018
Mathieu ROBERT <mathieu.robert@yocto.re>
2016-07-12 11:01:35 +0400

    Some change on code and more comments

--- index.js
+++ index.js
@@ -1,47 +1,72 @@
 'use strict';
 
-var app     = require('express')();
-var request = require('request');
-var chalk   = require('chalk');
-var os      = require('os');
-var moment  = require('moment');
+// Dependencies
+var app         = require('express')();
+var request     = require('request');
+var chalk       = require('chalk');
+var os          = require('os');
+var moment      = require('moment');
 
-var mode = process.env.APP_MODE || 'ping';
-var port = 3000;
-var host = 'http://pong-local';
-var interval = null;
-var delay = 5000;
+// config vars
+var mode        = process.env.APP_MODE || 'ping';
+var port        = 3000;
+var host        = 'http://pong-local';
+var interval    = null;
+var delay       = 5000;
 
+// Catch default / route to process a reponse
 app.get('/', function (req, res) {
+  // log a message
   console.log([ 'Receive a ping request from [', chalk.blue(req.ip), ']'].join(' '));
+  // process reponse
   res.send('OK at ' + moment().format('DD/MM/YYYY hh:mm:ss'));
 });
 
+// Main callback to send request on pong server
 function processRequest () {
+  // process request
   request.get([ host, port ].join(':'), function (err, response, body) {
+    // has any error ?
     if (err) {
-      console.log([ 'Cannot connect on host, retry with default interval will continue =>', err ].join(' '));
+      // log error
+      console.log([ 'Cannot connect on host, retry with default interval will continue =>',
+        err ].join(' '));
+      // interval is null ?
       if (interval === null) {
-       console.log('Pong is not available try to reconnect');
-       interval = setInterval(processRequest, delay);
+        // log retry start
+        console.log('Pong is not available try to reconnect');
+        // process interval to check status
+        interval = setInterval(processRequest, delay);
       }
     } else {
+      // log reponse
       console.log([ 'Received Reponse [', chalk.green(body), '] from pong server' ].join(' '));
+      // clear first interval
       clearInterval(interval);
+      // process a timeout to send default process for human reading
       var timeout = setTimeout(function () {
-       clearTimeout(timeout);
-       processRequest();
+        // clear tiemout
+        clearTimeout(timeout);
+        // process request
+        processRequest();
       }, delay);
     }
   });
 }
 
+// is mong mode ?
 if (mode === 'pong') {
- app.listen(port, function () {
-  console.log([ 'App [', chalk.green(mode), '] started and waiting request on port [', chalk.yellow(port), ']' ].join(' '));
- });
+  // wait and listen default port
+  app.listen(port, function () {
+    // log message
+    console.log([ 'App [', chalk.green(mode), '] started and waiting request on port [',
+      chalk.yellow(port), ']' ].join(' '));
+  });
 } else {
- console.log([ 'App [', chalk.green(mode), '] started and sending request on pong server on [', chalk.yellow(host), ']' ].join(' '));
- interval = setInterval(processRequest, delay);
+  // start interval to check connection
+  console.log([ 'App [', chalk.green(mode), '] started and sending request on pong server on [',
+    chalk.yellow(host), ']' ].join(' '));
+  // process interval
+  interval = setInterval(processRequest, delay);
 }
 
